03e98b9d7ee4e29499eee60374031580254cd941
skarakash <karakash.sergey@gmail.com>
2016-10-26 00:25:53 +0300

    Styling of completed/incompleted todos

--- components/todo_list.js
+++ components/todo_list.js
@@ -9,9 +9,9 @@ class TodoList extends Component{
                         {this.props.data.map((todo)=> {
                             return (
                                 <div key={todo.todoID}>
-                                    <span className="list-group-item">{todo.text}</span>
-                                    <button className="btn btn-success" onClick={() => this.props.actions.deleteTodo(todo.todoID) }>Delete</button>
-                                    <button className="btn btn-success" onClick={() => this.props.actions.completeTodo(todo.todoID)}>Complete</button>
+                                    <span className="list-group-item"  style={{ color: todo.completed ? 'blue': 'red' }} >{todo.text}</span>
+                                    <button className="btn btn-success" onClick={() => this.props.actions.deleteTodo(todo.todoID)}>Delete</button>
+                                    <button className="btn btn-success" onClick={() => this.props.actions.completeTodo(todo.todoID)}>{todo.completed ? 'Reopen': 'Complete'}</button>
                                 </div>
                             );
                         })}
