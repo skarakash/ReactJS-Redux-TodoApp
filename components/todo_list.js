62253c945296fbb87a2337449e6e9311f420a9d9
skarakash <karakash.sergey@gmail.com>
2016-11-03 21:51:07 +0200

    complete todo fix

--- components/todo_list.js
+++ components/todo_list.js
@@ -9,7 +9,7 @@ class TodoList extends Component{
                         {this.props.data.map((todo)=> {
                             return (
                                 <div key={todo.todoID}>
-                                    <span className="list-group-item"  style={{ color: todo.completed ? 'blue': 'red' }} >{todo.text}</span>
+                                    <span className="list-group-item"  style={{ textDecoration: todo.completed ? 'line-through': 'none' }} >{todo.text}</span>
                                     <button className="btn btn-success" onClick={() => this.props.actions.deleteTodo(todo.todoID)}>Delete</button>
                                     <button className="btn btn-success" onClick={() => this.props.actions.completeTodo(todo.todoID)}>{todo.completed ? 'Reopen': 'Complete'}</button>
                                 </div>
