87a77e3fec7ad9ad9afe2c01246db2ba0e74a5f4
skarakash <karakash.sergey@gmail.com>
2017-05-20 09:40:00 +0300

    router fix

--- components/Root.js
+++ components/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Wrapper from './todo_wrapper';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router'
 
 const Root = ({ store }) => (
     <Provider store={store}>
